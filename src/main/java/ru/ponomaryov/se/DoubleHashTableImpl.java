98d25d46d1b6eab39dddb11b7c1d3d6037aba976
Oleg A. Ponomaryov <35917357+SplitSplit@users.noreply.github.com>
2019-01-30 14:45:17 +0300

    Add files via upload

---
+++ src/main/java/ru/ponomaryov/se/DoubleHashTableImpl.java
@@ -0,0 +1,19 @@
+package ru.ponomaryov.se;
+
+public class DoubleHashTableImpl extends HashTableImpl{
+
+    private static int DOUBLE_HASH_CONST = 5;
+
+    public DoubleHashTableImpl(int maxSize) {
+        super(maxSize);
+    }
+
+    @Override
+    protected int getStep(int key) {
+        return hashFuncDouble(key);
+    }
+
+    private int hashFuncDouble(int key) {
+        return DOUBLE_HASH_CONST - (key % DOUBLE_HASH_CONST);
+    }
+}
